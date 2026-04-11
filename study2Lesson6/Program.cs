35e4e48eccf49cb45cb426aaecca83155e58d6e8
ixxmagicalx <ixxmagicalx_0723@qq.com>
2026-04-11 21:55:59 +0800

    C#基础关于特殊引用类型的学习

---
+++ study2Lesson6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("特殊引用类型");
+            #region 课
+            //string str1 = "123";
+            //string str2 = str1;
+            //Console.WriteLine(str1);
+            //Console.WriteLine(str2);
+            //Console.WriteLine("***********************");
+            //str2 = "456";
+            //Console.WriteLine(str1);
+            //Console.WriteLine(str2);
+            #endregion
+            #region 1.
+            int[] a = {10};
+            int[] b = a;
+            b = new int[5];//{0,0,0,0,0}
+            Console.WriteLine(a[0]);
+            #endregion
+        }
+    }
+}
